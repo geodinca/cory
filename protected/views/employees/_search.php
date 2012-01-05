1bc4d68564b097f79d0e2dfbbac066769cddc4c7
Marius Marin <mmc.pop3@gmail.com>
2012-01-05 22:54:07 +0200

    + autocomplete fields, multiple select

--- protected/views/employees/_search.php
+++ protected/views/employees/_search.php
@@ -2,32 +2,88 @@
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
+//	'method'=>'get',
+	'method'=>'post',
 )); ?>
 
 	<div class="row">
 		<?php echo Chtml::label('Boolean Search:', 'boolean_search'); ?>
-		<?php echo CHtml::textField('boolean_search', 'enter text...', array('size'=>50,'maxlength'=>256)); ?>
+		<?php echo CHtml::textField('Search[boolean_search]', 'enter text...', array('size'=>50,'maxlength'=>256)); ?>
 	</div>
 	
 	<div class="row">
 		<?php echo Chtml::label('Present Employer:', 'present_employer'); ?>
-		<?php echo CHtml::textField('present_employer', 'enter text...', array('size'=>50,'maxlength'=>256)); ?>
+		<?php echo CHtml::hiddenField('Search[present_employer]', null); ?>
+		<?php
+			$aCompanies = CHtml::ListData(Companies::model()->findAll(), 'id', 'name');
+			$this->widget('application.extensions.multicomplete.MultiComplete', array(
+					'updater' => 'Search_present_employer',
+					'splitter'=>',',
+					'source'=>$aCompanies,
+					'options'=>array(
+						'minLength'=>'2',
+					),
+					'htmlOptions'=>array(
+						'size'=>'60',
+			            'name' => 'present_employer',
+			          	'id' => 'present_employer'
+					),
+			  ));
+		?>
 	</div>
 	
 	<div class="row">
 		<?php echo Chtml::label('Present or Past Employer:', 'present_or_past_employer'); ?>
-		<?php echo CHtml::textField('present_or_past_employer', 'enter text...', array('size'=>50,'maxlength'=>256)); ?>
+		<?php
+			$this->widget('application.extensions.multicomplete.MultiComplete', array(
+					'splitter'=>',',
+					'source'=>$aCompanies,
+					'options'=>array(
+						'minLength'=>'2',
+					),
+					'htmlOptions'=>array(
+						'size'=>'60',
+			            'name' => 'Search[present_or_past_employer]',
+			          	'id' => 'Search_present_or_past_employer'
+					),
+			  ));
+		?>
 	</div>
 	
 	<div class="row">
 		<?php echo $form->label($model,'geographical_area'); ?>
-		<?php echo $form->textField($model,'geographical_area',array('size'=>60,'maxlength'=>255)); ?>
+		<?php
+			$this->widget('application.extensions.multicomplete.MultiComplete', array(
+					'splitter'=>',',
+					'source'=>CHtml::listData(Employees::model()->findAll(array('group' => 'geographical_area')), 'geographical_area', 'geographical_area'),
+					'options'=>array(
+						'minLength'=>'2',
+					),
+					'htmlOptions'=>array(
+						'size'=>'60',
+			            'name' => 'Search[geographical_area]',
+			          	'id' => 'Search_geographical_area'
+					),
+			  ));
+		?>
 	</div>
 	
 	<div class="row">
 		<?php echo Chtml::label('Countries AND/OR US States:', 'country_state'); ?>
-		<?php echo CHtml::textField('country_state', 'enter text...', array('size'=>50,'maxlength'=>256)); ?>
+		<?php
+			$this->widget('application.extensions.multicomplete.MultiComplete', array(
+					'splitter'=>',',
+					'source'=>CHtml::listData(Employees::model()->findAll(array('group' => 'geographical_area')), 'geographical_area', 'geographical_area'),
+					'options'=>array(
+						'minLength'=>'2',
+					),
+					'htmlOptions'=>array(
+						'size'=>'60',
+			            'name' => 'Search[country_state]',
+			          	'id' => 'Search_country_state'
+					),
+			  ));
+		?>
 	</div>
 	
 	<div class="row">
