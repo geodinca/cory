592b3e6789a58696711591ad514cf2e8eda33971
Marius Marin <mmc.pop3@gmail.com>
2012-01-10 22:33:36 +0200

    + new clients management system

---
+++ protected/views/clients/admin.php
@@ -0,0 +1,31 @@
+<?php
+$this->breadcrumbs=array(
+	'Clients'=>array('index'),
+	'Manage',
+);
+
+$this->menu=array(
+	array('label'=>'Create Client', 'url'=>array('create')),
+);
+?>
+
+<h1>Manage Clients</h1>
+
+<p>
+You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+</p>
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'clients-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'id',
+		'name',
+		array(
+			'class'=>'CButtonColumn',
+			'template' => '{update}{delete}'
+		),
+	),
+)); ?>
---
+++ protected/views/clients/update.php
@@ -0,0 +1,40 @@
+<?php
+$this->breadcrumbs=array(
+	'Clients'=>array('index'),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'Create Clients', 'url'=>array('create')),
+	array('label'=>'Manage Clients', 'url'=>array('admin')),
+);
+?>
+
+<h1>Update client: <b><?php echo CHtml::encode($model->name); ?></b></h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+
+<h2>Client instances</h2>
+<?php 
+// client instances
+$this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'instances-grid',
+	'dataProvider'=>$dataProvider,
+	'columns'=>array(
+		'id',
+		'name',
+		array(
+			'name' => 'created',
+			'type' => 'dateTime'
+		),
+		array(
+			'name' => 'expire',
+			'type' => 'dateTime'
+		),
+		array(
+			'class' => 'CButtonColumn',
+			'template' => '{edit}'
+		),
+	),
+)); 
+?>
