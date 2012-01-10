d1f8e01fe1937fe0256ce3cb322b3e2ec1e76757
Marius Marin <mmc.pop3@gmail.com>
2012-01-10 22:36:43 +0200

    + users, clints, instances, users_instances + all tables changed to MyISAM

--- protected/data/cory.mysql.sql
+++ protected/data/cory.mysql.sql
@@ -1,92 +1,144 @@
-CREATE  TABLE IF NOT EXISTS `cory`.`tbl_clients` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `users_id` INT NULL ,
-  `name` VARCHAR(255) NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_tbl_clients_tbl_users1` (`users_id` ASC) ,
-  CONSTRAINT `fk_tbl_clients_tbl_users1`
-    FOREIGN KEY (`users_id` )
-    REFERENCES `cory`.`tbl_users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 10, 2012 at 09:15 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
-CREATE  TABLE IF NOT EXISTS `cory`.`tbl_companies` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `street` VARCHAR(255) NULL ,
-  `city` VARCHAR(255) NULL ,
-  `country` VARCHAR(255) NULL ,
-  `state` VARCHAR(255) NULL ,
-  `zip` VARCHAR(255) NULL ,
-  `phone` VARCHAR(255) NULL ,
-  `web` VARCHAR(255) NULL ,
-  `products` TEXT NULL ,
-  `sales` VARCHAR(255) NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE  TABLE IF NOT EXISTS `cory`.`tbl_users` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(128) NULL ,
-  `password` VARCHAR(128) NULL ,
-  `email` VARCHAR(128) NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `cory`.`tbl_instances` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `users_id` INT NULL ,
-  `clients_id` INT NULL ,
-  `demo_title` VARCHAR(255) NULL ,
-  `application_title` VARCHAR(255) NULL ,
-  `expiration` DATETIME NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_tbl_instances_tbl_users1` (`users_id` ASC) ,
-  INDEX `fk_tbl_instances_tbl_clients1` (`clients_id` ASC) ,
-  CONSTRAINT `fk_tbl_instances_tbl_users1`
-    FOREIGN KEY (`users_id` )
-    REFERENCES `cory`.`tbl_users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_instances_tbl_clients1`
-    FOREIGN KEY (`clients_id` )
-    REFERENCES `cory`.`tbl_clients` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE  TABLE IF NOT EXISTS `cory`.`tbl_employees` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `companies_id` INT NULL ,
-  `instances_id` INT NULL ,
-  `name` VARCHAR(255) NULL ,
-  `title` VARCHAR(255) NULL ,
-  `geographical_area` VARCHAR(255) NULL ,
-  `contact_info` TEXT NULL ,
-  `email` VARCHAR(255) NULL ,
-  `home_street` VARCHAR(255) NULL ,
-  `home_city` VARCHAR(255) NULL ,
-  `home_state_country` VARCHAR(255) NULL ,
-  `home_zip` VARCHAR(255) NULL ,
-  `home_phone` VARCHAR(255) NULL ,
-  `actual_location_street` VARCHAR(255) NULL ,
-  `actual_location_city` VARCHAR(255) NULL ,
-  `actual_location_state` VARCHAR(255) NULL ,
-  `profile` TEXT NULL ,
-  `date_entered` DATETIME NULL ,
-  `date_update` DATETIME NULL ,
-  `misc_info` TEXT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_employees_companies` (`companies_id` ASC) ,
-  INDEX `fk_tbl_employees_tbl_instances1` (`instances_id` ASC) ,
-  CONSTRAINT `fk_employees_companies`
-    FOREIGN KEY (`companies_id` )
-    REFERENCES `cory`.`tbl_companies` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_employees_tbl_instances1`
-    FOREIGN KEY (`instances_id` )
-    REFERENCES `cory`.`tbl_instances` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Database: `cory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `street` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `city` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `state` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `zip` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `web` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `products` text CHARACTER SET latin1,
+  `sales` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=530 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `companies_id` int(11) NOT NULL,
+  `instances_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `title` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `geographical_area` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `contact_info` text CHARACTER SET latin1,
+  `email` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `home_street` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `home_city` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `home_state_country` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `home_zip` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `home_phone` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `actual_location_street` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `actual_location_city` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `actual_location_state` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `profile` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `date_entered` datetime NOT NULL,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `misc_info` text CHARACTER SET latin1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3145 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instances`
+--
+
+DROP TABLE IF EXISTS `instances`;
+CREATE TABLE IF NOT EXISTS `instances` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `client_id` int(10) unsigned NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expire` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instances_users`
+--
+
+DROP TABLE IF EXISTS `instances_users`;
+CREATE TABLE IF NOT EXISTS `instances_users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `instance_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(10) unsigned NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` char(32) NOT NULL,
+  `status` enum('enabled','disabled') NOT NULL DEFAULT 'disabled',
+  `type` enum('admin','client','guest') NOT NULL DEFAULT 'client',
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `expire` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
