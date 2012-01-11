f18f588c7b9644c0c125cefcddfc960eb0eafd69
geodinca <geodinca@gmail.com>
2012-01-11 12:55:41 +0200

    Add jeditable Notes to profile View

--- protected/views/employees/view.php
+++ protected/views/employees/view.php
@@ -31,11 +31,11 @@ $this->renderPartial('_menu',array('action'=>'selected_profile'));
 		<span><?php echo Yii::app()->format->html(nl2br($model->profile)); ?></span>
 		
 		<h3>Notes</h3>
-		<?php $form=$this->beginWidget('CActiveForm', array(
-			'action'=>Yii::app()->createUrl($this->route),
-			'method'=>'post',
-		)); ?>
-		<?php echo $form->textArea($model,'misc_info',array('rows'=>6, 'cols'=>118)); ?>
+		<?php //$form=$this->beginWidget('CActiveForm', array(
+// 			'action'=>Yii::app()->createUrl($this->route),
+// 			'method'=>'post',
+// 		)); ?>
+		<?php //echo $form->textArea($model,'misc_info',array('rows'=>6, 'cols'=>118)); ?>
 		<?php $this->widget(
 				'application.extensions.jeditable.DsJEditableWidget', 
 				array(
@@ -46,8 +46,8 @@ $this->renderPartial('_menu',array('action'=>'selected_profile'));
 						'cols'=> 118,
 						//'tooltip' => 'Hint message - press to add your custom notes to this profile'
 			)) ?>
-		<?php echo $form->error($model,'misc_info'); ?>
-		<?php $this->endWidget(); ?>
+		<?php //echo $form->error($model,'misc_info'); ?>
+		<?php //$this->endWidget(); ?>
 	</div>
 </div>
 <!-- PROFILE: START -->
