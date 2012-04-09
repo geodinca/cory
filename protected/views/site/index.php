82883e6d81c855f2baa0aca81f39f53589bc9f9c
Georgian Dinca <geo@magnify.ro>
2012-04-09 08:20:33 +0300

    * fix user access per instances - for admin

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -11,11 +11,11 @@
 <div id="static-page">
 	<h2>Select one of the following collections:</h2>
 	<?php
+		$accessInstances = array();
 		if(Yii::app()->user->credentials['type'] == 'admin'){
-			$aInstances = Instances::model()->findAll();
+			$accessInstances = Instances::model()->findAll();
 		} else {
 			$aInstances = InstancesUsers::model()->findAll('user_id = :uID', array(':uID' => Yii::app()->user->id));
-			$accessInstances = array();
 			foreach ($aInstances as $oInstance) {
 				$accessInstances[] = Instances::model()->find('id = :id', array(':id' => $oInstance->instance_id));
 			}
